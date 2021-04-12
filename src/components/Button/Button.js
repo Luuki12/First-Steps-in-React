b330fd638fe3e9eacf578cfa7d62a79e4b7049d6
Lukasz Kidon <lkidon4@gmail.com>
2021-04-12 22:17:43 +0200

    add card to column, column state change func, fixed all error

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Button.scss';
+import PropTypes from 'prop-types';
 
 const Button = ({variant = '', ...otherProps}) => (
+
+  Button.propTypes = {
+    variant: PropTypes.node,
+  },
 
   <button 
     {...otherProps}
     className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}
   />
+
 );
 
 export default Button;

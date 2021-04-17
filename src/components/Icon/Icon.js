9bff699bd00d3d8723b20fd0a32a4c5cbf8d7b98
Łukasz Kidoń <lkidon4@gmail.com>
2021-04-17 15:59:37 +0200

    add filter cards

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-Icon.propTypes = {
-  name: PropTypes.node,
-};
+
 
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
 

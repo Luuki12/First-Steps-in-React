26d85e7584486d25375500cdb4a4023d5215fd01
Lukasz Kidon <lkidon4@gmail.com>
2021-04-25 18:00:19 +0200

    add Container to Search and List Components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,7 @@ import Column from '../Column/ColumnContainer';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator';
-
+import Container from '../Container/Container';
 
 class List extends React.Component {
   
@@ -27,25 +27,28 @@ class List extends React.Component {
     const {title, image, description, columns, addColumn} = this.props;
 
     return (
-      <main className={styles.component}>
+      <Container>
+        <main className={styles.component}>
 
-        <Hero titleText={title} imageText={image} />
-        
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
+          <Hero titleText={title} imageText={image} />
 
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div> 
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
 
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
-        
-      </main>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div> 
+
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+
+        </main>
+
+      </Container>
     );
   }
 }

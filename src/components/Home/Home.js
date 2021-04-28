aaa5c34adb937339e9bf9953dd2204241f739076
Lukasz Kidon <lkidon4@gmail.com>
2021-04-28 19:50:07 +0200

    searchResult

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import styles from './Home.scss';
-import List from '../List/ListContainer';
+import ListLink from '../ListLink/ListLink';
 //import {settings} from '../../data/dataStore';
 import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer';
-
 
 class Home extends React.Component {
 
@@ -20,9 +18,8 @@ class Home extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         {lists.map(listData => (
-          <List key={listData.id} {...listData} />
+          <ListLink key={listData.id} {...listData} />
         ))}
       </main>
     );
